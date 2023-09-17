b3aefa2efae25e572f2f4e2115fc7007cf1a1329
Liviu Grozav <grozlivj@gmail.com>
2023-09-17 01:11:00 -0700

    Adding all practice files

---
+++ app.js
@@ -0,0 +1,31 @@
+// function hasOddNumber(arr) {
+//    return arr.some(function(number) {
+//       return (number % 2 === 1);
+//    });
+// };
+
+
+// function hasAZero(number) {
+// return number.toString().split('').some(function(num) {
+//       return num === '0';
+//    });
+// }
+
+// function hasOddNumber(arr) {
+//    return arr.every(function(number) {
+//       return (number % 2 === 1);
+//    });
+// };
+
+// function hasNoDuplicates(arr) {
+//    return arr.every(function(dup) {
+//       return arr.indexOf(dup) === arr.lastIndexOf(dup);
+//    });
+// }
+
+function hasCertainKey(arr, key) {
+   return arr.every(function(item) {
+      return item !== key;
+   });
+}
+
