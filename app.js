408cf5f8a7a2dfa71b2bcdbee51eab5f6e5cf76f
Liviu Grozav <grozlivj@gmail.com>
2023-09-17 01:13:14 -0700

    cleaner version of all practice files

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-// function hasOddNumber(arr) {
-//    return arr.some(function(number) {
-//       return (number % 2 === 1);
-//    });
-// };
+function hasOddNumber(arr) {
+   return arr.some(function(number) {
+      return (number % 2 === 1);
+   });
+};
 
 
-// function hasAZero(number) {
-// return number.toString().split('').some(function(num) {
-//       return num === '0';
-//    });
-// }
-
-// function hasOddNumber(arr) {
-//    return arr.every(function(number) {
-//       return (number % 2 === 1);
-//    });
-// };
-
-// function hasNoDuplicates(arr) {
-//    return arr.every(function(dup) {
-//       return arr.indexOf(dup) === arr.lastIndexOf(dup);
-//    });
-// }
-
-function hasCertainKey(arr, key) {
-   return arr.every(function(item) {
-      return item !== key;
+function hasAZero(number) {
+return number.toString().split('').some(function(num) {
+      return num === '0';
    });
 }
 
+function hasOddNumber(arr) {
+   return arr.every(function(number) {
+      return (number % 2 === 1);
+   });
+};
+
+function hasNoDuplicates(arr) {
+   return arr.every(function(dup) {
+      return arr.indexOf(dup) === arr.lastIndexOf(dup);
+   });
+}
+
+function hasCertainKey(arr, key) {
+   return arr.every(function(item) {
+      return key in item
+   });
+}
+
+function hasCertainValue(arr, key, val) {
+   return arr.every(function(val) {
+      return val[key] === val;
+   });
+}
